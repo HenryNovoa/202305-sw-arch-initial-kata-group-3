d8472b2b97158939b3589575526c782e691d7c07
Henry Novoa <henry.r.novoa@gmail.com>
2023-05-06 01:00:34 +0200

    Add initial config

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+// Without this file, eslint will not work with typescript, and gives weird errors
+
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['xo'],
+  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['**/dist/**/*.js'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    indent: ['Error', 2],
+    'object-curly-spacing': ['Error', 'always'],
+    'operator-linebreak': ['error', 'after'],
+  },
+};
